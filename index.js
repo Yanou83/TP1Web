3faf825bc1126f9c4a99741fe08ee98ddaebf0ba
MONTENOT <montenot.yann@gmail.com>
2025-01-20 12:13:11 +0100

    bottes alertes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('js lié avec succès');
+const boutonAjouterPanier1 = document.getElementById('btn1');
+const boutonAjouterPanier2 = document.getElementById('btn2');
+const boutonAjouterPanier3 = document.getElementById('btn3');
+
+console.log(boutonAjouterPanier1);
+
+
+boutonAjouterPanier1.addEventListener("click", ()=>{
+    alert('Botte ajoutée au panier avec succès');
+});
+
+boutonAjouterPanier2.addEventListener("click", ()=> {
+    alert('Botte ajoutée au panier avec succès');
+});
+
+boutonAjouterPanier3.addEventListener("click", ()=> {
+    alert('Botte ajoutée au panier avec succès');
+});
